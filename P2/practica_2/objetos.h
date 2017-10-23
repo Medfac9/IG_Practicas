923f9f7608a743de4ef9530f471c60e324bef71a
Medfac9 <ramefa@hotmail.com>
2017-10-23 18:12:52 +0200

    Práctica 2

---
+++ P2/practica_2/objetos.h
@@ -0,0 +1,69 @@
+//**************************************************************************
+// Práctica 2
+//**************************************************************************
+
+#include <vector>
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+#include "vertex.h"
+#include <stdlib.h>
+
+using namespace std;
+
+const float AXIS_SIZE=5000;
+
+//*************************************************************************
+// clase punto
+//*************************************************************************
+
+class _puntos3D
+{
+public:
+
+	_puntos3D();
+void 	draw_puntos(float r, float g, float b, int grosor);
+
+vector<_vertex3f> vertices;
+};
+
+//*************************************************************************
+// clase triángulo
+//*************************************************************************
+
+class _triangulos3D: public _puntos3D
+{
+public:
+
+	_triangulos3D();
+void 	draw_aristas(float r, float g, float b, int grosor);
+void  draw_solido(float r, float g, float b);
+void 	draw_solido_ajedrez(float r1, float g1, float b1, float r2, float g2, float b2);
+
+vector<_vertex3i> caras;
+};
+
+
+//*************************************************************************
+// clase cubo
+//*************************************************************************
+
+class _cubo: public _triangulos3D
+{
+public:
+
+	_cubo(float tam=3);
+};
+
+//*************************************************************************
+// clase piramide
+//*************************************************************************
+
+class _piramide: public _triangulos3D
+{
+public:
+
+	_piramide(float tam=3, float al=5);
+};
